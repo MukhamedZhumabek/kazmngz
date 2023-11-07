c2a63d6ab498d29baf999e4ad696fc63e9d91117
MukhamedZhumabek <mukhamedzh@gmail.com>
2023-11-07 14:10:05 +0600

    update_start_script

--- start.sh
+++ start.sh
@@ -1,7 +1,21 @@
 #!/bin/bash
-
+mkdir logs
 touch logs/generator.log
 touch logs/dev0.log
 touch logs/dev1.log
 touch logs/dev2.log
 
+git clone https://github.com/MukhamedZhumabek/data_generator.git
+git clone https://github.com/MukhamedZhumabek/dev0.git
+git clone https://github.com/MukhamedZhumabek/dev1.git
+git clone https://github.com/MukhamedZhumabek/dev2.git
+
+mkdir data_generator/log
+mkdir dev0/log
+mkdir dev1/log
+mkdir dev2/log
+
+touch data_generator/log/generator.log
+touch dev0/log/dev0.log
+touch dev1/logs/dev1.log
+touch dev2/logs/dev2.log
